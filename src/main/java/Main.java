381c50c988bd261c982375a635c8e584bfb642d6
Dheeraj Bishnoi <engineerdheerajbishnoi@gmail.com>
2024-12-05 12:55:37 +0530

    add

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.year2024.Dec4Easy;
+import org.year2024.Nov30Hard;
+
+public class Main {
+    public static void main(String[] args) {
+        Nov30Hard obj = new Nov30Hard();
+        obj.solve();
+    }
+}
---
+++ src/main/java/org/year2024/Dec3Med.java
@@ -0,0 +1,30 @@
+package org.year2024;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/adding-spaces-to-a-string/
+
+public class Dec3Med {
+    public String addSpaces(String s, int[] spaces) {
+        List<String> lst = new ArrayList<>();
+        for(int i=0; i<s.length(); i++) {
+            lst.add(String.valueOf(s.charAt(i)));
+        }
+
+        for(int idx: spaces) {
+            String ss = " "+lst.get(idx);
+            lst.set(idx, ss);
+        }
+
+        return String.join("", lst);
+    }
+
+    public void solve() {
+//        String ans = addSpaces("abc", new int[]{0, 2});
+//        String ans = addSpaces("LeetcodeHelpsMeLearn", new int[]{8,13,15});
+//        String ans = addSpaces("icodeinpython", new int[]{1,5,7,9});
+        String ans = addSpaces("spacing", new int[]{0,1,2,3,4,5,6});
+        System.out.println(ans);
+    }
+}
